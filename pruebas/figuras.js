55bd6fcacf548565ddf9fa2e80af791ef4551014
maofong <maofong@gmail.com>
2021-11-23 18:32:09 -0300

    2

--- pruebas/figuras.js
+++ pruebas/figuras.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden " + ladoCuadrado +"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el perimetro del cuadrado es " + perimetroCuadrado +"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el area del cuadrado es " + areaCuadrado +"cm2");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 4;
+
+const areaTriangulo = ladoTriangulo1+ladoTriangulo2+ladoTriangulo3;
+
+console.log("los lados del triangulo miden "+ 
+    ladoTriangulo1+ " "+
+    ladoTriangulo2+ " "+
+    ladoTriangulo3+ "siendo su area " +
+    areaTriangulo
+    );
